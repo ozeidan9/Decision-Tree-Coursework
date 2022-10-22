c39a7a5294dc4dd6db435cdfe4052fe32d491976
Shaanukstar123 <shaanukag0@gmail.com>
2022-10-22 03:54:52 +0100

    testing recursive approach

--- src/prune.py
+++ src/prune.py
@@ -1,8 +1,15 @@
-#pruning the decision tree that's passed into the function
-def pruning (tree, node, test_data):
-    if not tree or (not tree['right'] and not tree['left']):
+#Recursively traverses the tree and prunes it by evaluating the accuracy
+def pruning (tree,root):
+
+    #returns tree if either the tree is Null or both leaf nodes are Null
+    if not tree or (not tree["right"] and not tree["left"]):
         return tree
     
 
-    
+    right_copy = tree["right"]
+    left_copy = tree["left"]
+
+    left = pruning(tree["left"],root)
+    right = pruning(tree["right"],root)
+
     
