9b173c4a1cd6b67cdf6426364a1d542286e87ab6
ozeidan9 <omar.zeidan20@imperial.ac.uk>
2022-10-26 12:38:15 +0100

    Updated tree traversal function

--- src/crosseval.py
+++ src/crosseval.py
@@ -1,20 +1,22 @@
 
 class eval():
     def __init__(self):
-        self.leaf_nodes = []
+        pass
+    
     
     def eval_tree(self, root):
         """ DFS traversal through decision tree and prins leaf nodes
 
         Input: root node (type: tree)
+        return an integer
         """
         if not root.node['left'] and not root.node['right']: # leaf node
-            self.leaf_nodes.append(root.node['attribute'])
-            return
+            return root.node['attribute']
 
-        self.eval_tree(root.node['left'])
-        self.eval_tree(root.node['right'])
-        return self.leaf_nodes
+        if root.node['val']<=root.node['attribute']:
+            return self.eval_tree(root.node['left'])
+        else:    
+            return self.eval_tree(root.node['right'])
     
     
     
