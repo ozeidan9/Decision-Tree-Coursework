c092557ea0396eca4508c92cd5638fdc3cedfd8a
Shaheen <Flamebull5@gmail.com>
2022-10-26 17:00:13 +0100

    Fixed crosseval

--- src/crosseval.py
+++ src/crosseval.py
@@ -1,22 +1,21 @@
-
+import numpy as np
 class eval():
-    def __init__(self):
-        pass
-    
-    
-    def eval_tree(self, root):
+    def eval_tree(self, root, input):
         """ DFS traversal through decision tree and prins leaf nodes
 
         Input: root node (type: tree)
         return an integer
         """
-        if not root.node['left'] and not root.node['right']: # leaf node
+        # leaf node
+        if not root.node['left'] and not root.node['right']: 
             return root.node['attribute']
+        
 
-        if root.node['val']<=root.node['attribute']:
-            return self.eval_tree(root.node['left'])
+        attr = root.node['attribute']
+        if input[attr] <= root.node['val']:
+            return self.eval_tree(root.node['left'], input)
         else:    
-            return self.eval_tree(root.node['right'])
+            return self.eval_tree(root.node['right'], input)
     
     
     
