cd18b89ea5b4f66c80118a151e09a60179ce3892
Aeroking21 <55886814+Aeroking21@users.noreply.github.com>
2022-11-01 16:54:48 +0000

    pruning syntax change

--- src/prune.py
+++ src/prune.py
@@ -16,55 +16,60 @@ def pruning(head, root, depth):
 
     #evaluation_du.accuracy_validation(root)
 
-    if(root['val'] == None):
-        return 0
+    if(root.node['val'] == None):
+        return (root, 0)
 
-    left = pruning(head, root["left"], depth)
-    right = pruning(head, root["right"], depth)
+    left, ldepth = pruning(head, root.node["left"], depth)
+    right, rdepth = pruning(head, root.node["right"], depth)
+
+    root.node["left"] = left
+    root.node["right"] = right
 
     is_pruned = False;
 
-    if(root["left"]["left"] == None and root["left"]["right"] == None and root["right"]["left"] == None and root["right"]["right"] == None ):
+
+    if(root.node["left"]["left"] == None and root.node["left"]["right"] == None and root.node["right"]["left"] == None and root.node["right"]["right"] == None):
         
         reference_accuracy = Evaluation(head)
         
-        left_subtree = dict(root["left"])
-        left_class = root["left"]["attribute"]
-        right_subtree = dict(root["right"])
-        right_class = root["right"]["attribute"]
-        curr_val = root["val"]
+        left_subtree = dict(root.node["left"])
+        left_class = root.node["left"]["attribute"]
+        right_subtree = dict(root.node["right"])
+        right_class = root.node["right"]["attribute"]
+        curr_val = root.node["val"]
 
-        root["left"] = None
-        root["right"] = None
-        root["val"] = None
+        root.node["left"] = None
+        root.node["right"] = None
+        root.node["val"] = None
 
         #Repace with left
-        root["attribute"] = left_class
+        root.node["attribute"] = left_class
         left_acc = Evaluation(head)
 
         #Repace with right
-        root["attribute"] = right_class
+        root.node["attribute"] = right_class
         right_acc = Evaluation(head)
 
         if(left_acc >= reference_accuracy):
             if(left_acc > right_acc):
                 is_pruned = True
-                root["attribute"] = left_class
+                root.node["attribute"] = left_class
          
         elif(right_acc >= reference_accuracy):
             is_pruned = True
-            root["attribute"] = right_class
+            root.node["attribute"] = right_class
 
         else:
-            root["val"] = curr_val
-            root["left"] = left_subtree
-            root["right"] = right_subtree
+            root.node["val"] = curr_val
+            root.node["left"] = left_subtree
+            root.node["right"] = right_subtree
 
 
     if(is_pruned):
-        return 1
+        return (root, 0)
 
-    return 1 + max(left, right)
+
+    return (root, 1 + max(ldepth, rdepth))
     
 
 
